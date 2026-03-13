011ac1d0d15acf51499181db54c752afeb1700de
Lukiiy <102190774+Lukiiy@users.noreply.github.com>
2026-03-12 21:40:17 -0300

    Update main.py
    * Changed some arguments' descriptions

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ def parseArguments():
     p = argparse.ArgumentParser(description="Set Discord RPC")
 
     p.add_argument("--client-id", required = True, help = "Client ID")
-    p.add_argument("--details", default = "", help = "Main details text (e.g. \"Playing Foo\")")
+    p.add_argument("--details", default = "", help = "Main details text (e.g. \"Playing Game\")")
     p.add_argument("--state", default = "", help = "Secondary text")
-    p.add_argument("--large-image", default = None, help = "Key of the large image asset in your app")
+    p.add_argument("--large-image", default = None, help = "Key of the large image asset")
     p.add_argument("--large-text", default = None, help = "Hover text for the large image")
     p.add_argument("--small-image", default = None, help = "Key of the small image asset")
     p.add_argument("--small-text", default = None, help = "Hover text for the small image")
-    p.add_argument("--once", action = "store_true", help = "Set presence once and exit")
+    p.add_argument("--once", action = "store_true", help = "Execute and exit")
     p.add_argument("--retry", type = int, default = 5, help = "Seconds to wait before reconnect attempts")
 
     return p.parse_args()
